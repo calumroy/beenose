588ce3262d2e45aea1c82c06d4d50be4d2b0c1c3
calum\roy <calum047@gmail.com>
2020-05-16 14:48:28 +0800

    Add ability to right click and make new creator nodes. Needs work the location is wrong.

--- CreatorSprite.gd
+++ CreatorSprite.gd
@@ -1,5 +1,6 @@
 extends Sprite
 
+class_name CreatorSprite
 
 # Declare member variables here. 
 # Get the conway grid simulation Node
@@ -13,13 +14,13 @@ func _ready():
 	print("Creator ", node_par.name)
 	# Scale the node to be the same size as a conway square
 	conway_sq_size = global_par.size_sq
-	var sprite_size = texture.get_size()
-	#Transform.Scale.x = (conway_sq_size.x/sprite_size.x)
-	#Transform.Scale.y = (conway_sq_size.y/sprite_size.y)
-	print("Before scale transform ", node_par.position)
-	var scale_size = conway_sq_size/sprite_size
-	set_scale(conway_sq_size/sprite_size)
-	print("After scale transform ", node_par.position)
+#	var sprite_size = texture.get_size()
+#	#Transform.Scale.x = (conway_sq_size.x/sprite_size.x)
+#	#Transform.Scale.y = (conway_sq_size.y/sprite_size.y)
+#	print("Before scale transform ", node_par.position)
+#	var scale_size = conway_sq_size/sprite_size
+#	set_scale(conway_sq_size/sprite_size)
+#	print("After scale transform ", node_par.position)
 	
 
 
